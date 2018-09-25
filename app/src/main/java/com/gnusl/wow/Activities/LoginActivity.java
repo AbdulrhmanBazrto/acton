d8bc2452210fc9e5d54b08546671bcda1154ea1e
YEHIA ITE <yehia.ite@gmail.com>
2018-09-25 23:17:48 +0300

    handle entrance to the app

--- app/src/main/java/com/gnusl/wow/Activities/LoginActivity.java
+++ app/src/main/java/com/gnusl/wow/Activities/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.gnusl.wow.Activities;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +13,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        findViewById(R.id.wow_login_icon).setOnClickListener(v->{
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                    finish();
+                }
+            },500);
+        });
     }
 }
--- app/src/main/java/com/gnusl/wow/Activities/SplashActivity.java
+++ app/src/main/java/com/gnusl/wow/Activities/SplashActivity.java
@@ -1,5 +1,9 @@
 package com.gnusl.wow.Activities;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +15,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        // go to login activity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,LoginActivity.class));
+                finish();
+            }
+        },3500);
     }
 }
