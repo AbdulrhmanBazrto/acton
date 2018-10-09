997d4d665516f58006b56037baef54e9e5d71c0c
yehia_ite <yehia.ite@gmail.com>
2018-10-09 21:00:47 +0300

    finish top bar chat room

---
+++ app/src/main/java/com/gnusl/wow/Activities/RoomChatActivity.java
@@ -0,0 +1,16 @@
+package com.gnusl.wow.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.gnusl.wow.R;
+
+public class RoomChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room_chat);
+
+    }
+}
