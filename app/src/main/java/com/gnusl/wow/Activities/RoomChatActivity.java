2ba51f8f61f41e64d712eb0730f69b26a2fec76c
Yehia_talas <yehia.talas@medicus.ai>
2018-10-12 01:20:53 +0300

    complete score bar in chat room

--- app/src/main/java/com/gnusl/wow/Activities/RoomChatActivity.java
+++ app/src/main/java/com/gnusl/wow/Activities/RoomChatActivity.java
@@ -2,8 +2,15 @@ package com.gnusl.wow.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import com.gnusl.wow.Adapters.UsersRecyclerViewAdapter;
+import com.gnusl.wow.Adapters.UsersScoreRoomRecyclerViewAdapter;
+import com.gnusl.wow.Models.User;
 import com.gnusl.wow.R;
+
+import java.util.ArrayList;
 
 public class RoomChatActivity extends AppCompatActivity {
 
@@ -12,5 +19,32 @@ public class RoomChatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room_chat);
 
+        // score user
+        initializeUsersScore();
+
+
+
+    }
+
+    private void initializeUsersScore(){
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.score_user_recycler_view);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        ArrayList<User> users = new ArrayList<>();
+        users.add(new User(R.drawable.img1));
+        users.add(new User(R.drawable.img1));
+        users.add(new User(R.drawable.img1));
+        users.add(new User(R.drawable.img1));
+        users.add(new User(R.drawable.img1));
+        users.add(new User(R.drawable.img1));
+        users.add(new User(R.drawable.img1));
+
+
+        UsersScoreRoomRecyclerViewAdapter usersScoreRoomRecyclerViewAdapter= new UsersScoreRoomRecyclerViewAdapter(this, users);
+        recyclerView.setAdapter(usersScoreRoomRecyclerViewAdapter);
     }
 }
